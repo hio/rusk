cb1853a31723c1174cb0c47ade710ce1e694a3d2
YAMASHINA Hio <hio@hio.jp>
2019-12-01 20:09:29 +0900

    render tables in correct widths.

--- src/formatter/markdown.rs
+++ src/formatter/markdown.rs
@@ -1,5 +1,6 @@
 //! # Markdown formatter
-use crate::formatter::doc::{ Doc };
+use crate::formatter::doc::{ Doc, text_width };
+use std::rc::Rc;
 
 
 struct Opts
@@ -24,6 +25,14 @@ impl Opts
 			in_code: true,
 		}
 	}
+}
+
+
+#[derive(Debug)]
+struct RenderingCell
+{
+	s: String,
+	width: usize,
 }
 
 
@@ -92,32 +101,7 @@ impl WriteMarkdownText for Doc
 			Doc::SepEndBy(ref sep, ref end, ref v) =>
 				sep_end_by(f, &Doc::Static(sep), &Doc::Static(end), v, opts),
 
-			Doc::Table(ref header, ref rows) => {
-				write!(f, "|")?;
-				for elem in header.as_ref()
-				{
-					write!(f, " ")?;
-					elem.encode(f, opts)?;
-					write!(f, " |")?;
-				}
-				write!(f, "\n")?;
-
-				write!(f, "|{}\n", String::from("---|").repeat(header.len()))?;
-
-				for row in rows.iter()
-				{
-					write!(f, "|")?;
-					for elem in row.as_ref()
-					{
-						write!(f, " ")?;
-						elem.encode(f, opts)?;
-						write!(f, " |")?;
-					}
-					write!(f, "\n")?;
-				}
-
-				Ok(())
-			},
+			Doc::Table(ref header, ref rows) => write_table(f, opts, header, rows),
 
 			Doc::Cell(ref v) => {
 				//XXX: should be processed by inner works.
@@ -171,5 +155,66 @@ fn escape(f: &mut impl std::fmt::Write, s: &String, opts: &Opts) -> std::fmt::Re
 			}
 		}
 	}
+	Ok(())
+}
+
+
+impl RenderingCell
+{
+	fn from_doc(doc: &Doc, opts: &Opts) -> RenderingCell
+	{
+		let mut s = String::new();
+		doc.encode(&mut s, opts).unwrap();
+		let width = text_width(&s);
+		RenderingCell { s, width }
+	}
+}
+
+
+fn write_table(f: &mut impl std::fmt::Write, opts: &Opts, header: &Rc<Vec<Doc>>, rows: &Rc<Vec<Rc<Vec<Doc>>>>) -> std::fmt::Result
+{
+	let header_cells = header.iter().map(
+		|doc_cell| RenderingCell::from_doc(doc_cell, opts)
+	).collect::<Vec<_>>();
+	let body_cells = rows.iter().map(
+		|doc_row| doc_row.iter().map(
+			|doc_cell| RenderingCell::from_doc(doc_cell, opts)
+		).collect::<Vec<_>>()
+	).collect::<Vec<_>>();
+
+	let widths = body_cells.iter().fold(
+		header_cells.iter().map(|cell| cell.width).collect::<Vec<_>>(),
+		|acc, row| {
+			acc.iter()
+				.zip(row.iter().map(|cell| cell.width))
+				.map(|(a, b)| if *a > b { *a } else { b })
+				.collect::<Vec<_>>()
+		},
+	);
+
+	write!(f, "|")?;
+	for (i, cell) in header_cells.iter().enumerate()
+	{
+		write!(f, " {1:0$} |", widths[i], cell.s)?;
+	}
+	write!(f, "\n")?;
+
+	write!(f, "|")?;
+	for width in &widths
+	{
+		write!(f, "{1:-<0$}|", width + 2, "")?;
+	}
+	write!(f, "\n")?;
+
+	for row in body_cells.iter()
+	{
+		write!(f, "|")?;
+		for (i, cell) in row.iter().enumerate()
+		{
+			write!(f, " {1:0$} |", widths[i], cell.s)?;
+		}
+		write!(f, "\n")?;
+	}
+
 	Ok(())
 }
